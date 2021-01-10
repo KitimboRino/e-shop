ef44f34bb0b58a236dba63ad77b32505245fa201
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2021-01-10 19:02:24 +0300

    Preparing the API server

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors());
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+// Routers
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+// const Product = require('./models/product');
+
+// Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Database is connected');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+// Connecting to port 3000
+app.listen(3000, () => {
+  console.log(api);
+  console.log('server is running http://localhost:3000');
+});
