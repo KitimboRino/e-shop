017cda738146f0cc3bf7a51ed063138cfcfb2049
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2021-01-11 23:29:33 +0300

    Creating User model & Schema

--- models/user.js
+++ models/user.js
@@ -1,7 +1,65 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  passwordHarsh: {
+    type: String,
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    required: true,
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+
+  street: {
+    type: String,
+    default: '',
+  },
+
+  apartment: {
+    type: String,
+    default: '',
+  },
+
+  zip: {
+    type: String,
+    default: '',
+  },
+
+  city: {
+    type: String,
+    default: '',
+  },
+
+  country: {
+    type: String,
+    default: '',
+  },
+});
+
+
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set('toJSON', {
+  virtuals: true,
 });
 
 module.exports = mongoose.model('User', userSchema);
+exports.userSchema = userSchema;
