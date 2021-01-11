7acb35491b85584af74c656a5c25fd39bcaff869
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2021-01-11 16:44:48 +0300

    Get Featured Products

--- routes/products.js
+++ routes/products.js
@@ -116,4 +116,16 @@ router.get('/get/count', async (req, res) => {
   });
 });
 
+// Featured Products
+router.get('/get/featured/:count', async (req, res) => {
+  const count = req.params.count ? req.params.count : 0;
+  const products = await Product.find({ isFeatured: true }).limit(+count);
+
+  if (!products) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(products);
+});
+
 module.exports = router;
